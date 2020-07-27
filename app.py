83a39416be9703bf601d8d39ed892176df4c0288
NolanGC <nolangclement@gmail.com>
2020-07-26 23:19:41 -0700

    RJIOPHQEWGPIO

--- app.py
+++ app.py
@@ -13,55 +13,7 @@ from torch.utils.data import DataLoader
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
     return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-class KeanuModel(nn.Module):
-    def __init__(self, input_size, hidden_size, out_size):
-        super().__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
-        self.linear2 = nn.Linear(hidden_size, hidden_size2)
-        self.linear3 = nn.Linear(hidden_size2, hidden_size3)
-        self.linear4 = nn.Linear(hidden_size3, num_classes)
-
-    def forward(self, xb):
-        # Flatten the image tensors
-        xb = xb.view(xb.size(0), -1)
-        # Get intermediate outputs using hidden layer
-        out = self.linear1(xb)
-        # Apply activation function
-        out = F.relu(out)
-        # Get predictions using output layer
-        out = self.linear2(out)
-        out = F.relu(out)
-        out = self.linear3(out)
-        out = F.relu(out)
-        out = self.linear4(out)
-        return out
-
-    def training_step(self, batch):
-        images, labels = batch
-        out = self(images)  # Generate predictions
-        loss = F.cross_entropy(out, labels)  # Calculate loss
-        return loss
-
-    def validation_step(self, batch):
-        images, labels = batch
-        out = self(images)  # Generate predictions
-        loss = F.cross_entropy(out, labels)  # Calculate loss
-        acc = accuracy(out, labels)  # Calculate accuracy
-        return {"val_loss": loss, "val_acc": acc}
-
-    def validation_epoch_end(self, outputs):
-        batch_losses = [x["val_loss"] for x in outputs]
-        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
-        batch_accs = [x["val_acc"] for x in outputs]
-        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
-        return {"val_loss": epoch_loss.item(), "val_acc": epoch_acc.item()}
-
-    def epoch_end(self, epoch, result):
-        print(
-            "Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(
-                epoch, result["val_loss"], result["val_acc"]
-            )
-        )
+    
 input_size = 3 * 64 * 64
 num_classes = 2
 hidden_size = 64
