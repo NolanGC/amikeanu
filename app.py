1ed272dfe052e8abc3cacd602f2745c7299d7dff
NolanGC <nolangclement@gmail.com>
2020-07-27 01:23:16 -0700

    werij

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import numpy as np
 from PIL import Image
 import torch.nn as nn
 import torch.nn.functional as F
-from keanumodel import KeanuModel
 import torchvision.transforms as transforms
 from flask import Flask, request, render_template
+from keanumodel import KeanuModel
 app = Flask(__name__) 
 
 from commons import get_tensor
@@ -17,7 +17,7 @@ from inference import get_pred
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "images")
 
 @app.route("/", methods=["GET", "POST"])
-def hello_world():
+def site():
     if request.method == "GET":
         return render_template("index.html", value="hi")
     if request.method == "POST":
@@ -33,3 +33,5 @@ def hello_world():
             pred=str(round(pred * 100, 2)),
             image=os.path.join(app.config["UPLOAD_FOLDER"], filename),
         )
+if __name__ == "__main__":
+    app.run(debug=True)
