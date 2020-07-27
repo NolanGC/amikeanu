4f8927707a8b19b20302bca06ce5663086c9264c
NolanGC <nolangclement@gmail.com>
2020-07-27 00:57:58 -0700

    ewitjf

--- app.py
+++ app.py
@@ -16,25 +16,6 @@ from inference import get_pred
 
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "images")
 
-class KeanuModel(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.linear1 = nn.Linear(3 * 64 * 64, 32)
-        self.linear2 = nn.Linear(32, 32)
-        self.linear3 = nn.Linear(32, 16)
-        self.linear4 = nn.Linear(16, 2)
-
-    def forward(self, xb):
-        xb = xb.view(xb.size(0), -1)
-        out = self.linear1(xb)
-        out = F.relu(out)
-        out = self.linear2(out)
-        out = F.relu(out)
-        out = self.linear3(out)
-        out = F.relu(out)
-        out = self.linear4(out)
-        return out
-
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     if request.method == "GET":
@@ -52,8 +33,3 @@ def hello_world():
             pred=str(round(pred * 100, 2)),
             image=os.path.join(app.config["UPLOAD_FOLDER"], filename),
         )
-if __name__ == "__main__":
-    model = KeanuModel()
-    KeanuModel.__module__ = "keanumodel"
-    model.load_state_dict(torch.load(os.path.join(os.path.join("static", "images"), "checkpoint.pth"))["state_dict"])
-    app.run(debug=True)
