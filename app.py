d63af6fcde99f29616b6979560cac482588827d5
NolanGC <nolangclement@gmail.com>
2020-07-28 11:42:07 -0700

    ewhiot

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 import torchvision.transforms as transforms
 from flask import Flask, request, render_template
 app = Flask(__name__) 
@@ -17,6 +18,12 @@ app.config["UPLOAD_FOLDER"] = os.path.join("static", "images")
 from keanumodel import KeanuCnnModel
 
 pth_path = os.path.join(os.path.join("static", "images"), "keanucnn1.pth")
+def confidence(x):
+    if(x < 0.65):
+        return min(1, x*np.exp(-3*(x-0.65)**2))
+    else:
+        return x*np.exp(3*(x-0.65)**2)
+    
 #model.load_state_dict(torch.load(pth_path)["state_dict"])
 #model = torch.load(pth_path)
 #torch.save(model, "model2.pth")
@@ -35,10 +42,12 @@ def site():
         file = request.files["file"]
         image = file.read()
         pred = get_pred(image, model)
+        mathpred = confidence(pred)
         filename = "keanu.gif"
         return render_template(
             "result.html",
-            pred=str(round(pred * 100, 2)),
+            pred=str(round(mathpred * 100, 2)),
+            #otherpred = str(round(mathpred*100,2)),
             image=os.path.join(app.config["UPLOAD_FOLDER"], filename),
         )
 if __name__ == "__main__":
