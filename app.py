2897b2dd7db38a29efc944b60c3165cfa3644f3d
NolanGC <nolangclement@gmail.com>
2020-07-29 11:10:25 -0700

    final

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
+import io
 import os
 import cv2
+import math
 import torch
-import io
 import numpy as np
 from PIL import Image
 import torch.nn as nn
 import torch.nn.functional as F
-import math
 import torchvision.transforms as transforms
 from flask import Flask, request, render_template
+
 app = Flask(__name__) 
 
 from inference import get_pred
 
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "images")
-#from keanumodel import KeanuCnnModel
 
-#pth_path = os.path.join(os.path.join("static", "models"), "keanu_resnet.pth")   
-#model.load_state_dict(torch.load(pth_path)["state_dict"])
-#model = torch.load(pth_path)
-#torch.save(model, "model2.pth")
-#model = KeanuCnnModel()
-#model.load_state_dict(torch.load('cnnstatedict.pth'))
-#model = torch.load('model2.pth')
 model = torch.load('model.pth')
 model.eval()
 
@@ -35,15 +28,11 @@ def site():
             print("no files")
             return
         file = request.files["file"]
-        image = file.read()
-        pred = get_pred(image, model)
-        filename = "keanu.gif"
+        pred = get_pred(file.read(), model)
         return render_template(
             "result.html",
-            #pred=str(round(pred*100,2)),
             pred=str(round(pred*100,2)),
-            #otherpred = str(round(mathpred*100,2)),
-            image=os.path.join(app.config["UPLOAD_FOLDER"], filename),
+            image=os.path.join(app.config["UPLOAD_FOLDER"], "keanu.gif"),
         )
 if __name__ == "__main__":
     app.run(debug=True)
--- inference.py
+++ inference.py
@@ -2,28 +2,12 @@ import io
 import os
 import cv2
 import torch
+import numpy as np
 from PIL import Image
+import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
-import torch.nn as nn
-import numpy as np
 import torchvision.transforms as transforms
-
-# def get_tensor(image_bytes):
-#     my_transforms = transforms.Compose([
-#         transforms.Resize(256),
-#         transforms.CenterCrop(224),
-#         transforms.ToTensor(),
-#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#     ])
-#     pil_image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-#     open_cv_image = np.array(pil_image)
-#     open_cv_image = open_cv_image[:, :, ::-1].copy()
-#     new_array = cv2.resize(open_cv_image, (224,224))
-#     new_array = np.array(new_array[:, :, [2, 1, 0]])
-#     new_array = np.transpose(new_array, (2, 0, 1))
-#     img_tensor = torch.tensor(new_array, dtype=torch.float)
-#     return img_tensor
 
 def image_loader(loader, image_bytes):
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
@@ -39,21 +23,6 @@ def get_pred(image_bytes, model):
          transforms.ToTensor(),
          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
-    #print(model(F.softmax(image_loader(data_transforms, image_bytes),dim=1)[0]))
     pred = torch.tensor(model(image_loader(data_transforms, image_bytes)).detach().numpy())
     probs = F.softmax(pred).tolist()[0]
     return probs[0]
-    # tensor = get_tensor(image_bytes)
-    # print(tensor.shape)
-    # test_loader = DataLoader([(tensor, 0)], 1)
-    # for img, label in test_loader:
-    #     outputs = model(img)
-    #     _, preds = torch.max(outputs, 1)
-    #     print("Label: ",label)
-    # print("Softmax: ", F.softmax(outputs, dim=1))
-    # print("preds; ", preds)
-    #probs = F.softmax(pred, dim=1)[0].tolist()
-    #print(probs)
-    #print(probs)
-    #return probs[1]
-    
