a91d8741d96af29808502f1f26122ad3e6063fec
NolanGC <nolangclement@gmail.com>
2020-07-29 01:23:57 -0700

    good model

--- app.py
+++ app.py
@@ -11,25 +11,20 @@ import torchvision.transforms as transforms
 from flask import Flask, request, render_template
 app = Flask(__name__) 
 
-from commons import get_tensor
 from inference import get_pred
 
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "images")
-from keanumodel import KeanuCnnModel
+#from keanumodel import KeanuCnnModel
 
-pth_path = os.path.join(os.path.join("static", "images"), "keanucnn1.pth")
-def confidence(x):
-    if(x < 0.65):
-        return min(1, x*np.exp(-3*(x-0.65)**2))
-    else:
-        return x*np.exp(3*(x-0.65)**2)
-    
+#pth_path = os.path.join(os.path.join("static", "models"), "keanu_resnet.pth")   
 #model.load_state_dict(torch.load(pth_path)["state_dict"])
 #model = torch.load(pth_path)
 #torch.save(model, "model2.pth")
-model = KeanuCnnModel()
-model.load_state_dict(torch.load('cnnstatedict.pth'))
+#model = KeanuCnnModel()
+#model.load_state_dict(torch.load('cnnstatedict.pth'))
 #model = torch.load('model2.pth')
+model = torch.load('model.pth')
+model.eval()
 
 @app.route("/", methods=["GET", "POST"])
 def site():
@@ -42,13 +37,11 @@ def site():
         file = request.files["file"]
         image = file.read()
         pred = get_pred(image, model)
-        mathpred = round(confidence(pred)*100,2)
-        if(mathpred > 100):
-            mathpred = 100
         filename = "keanu.gif"
         return render_template(
             "result.html",
-            pred=str(mathpred),
+            #pred=str(round(pred*100,2)),
+            pred=str(round(pred*100,2)),
             #otherpred = str(round(mathpred*100,2)),
             image=os.path.join(app.config["UPLOAD_FOLDER"], filename),
         )
